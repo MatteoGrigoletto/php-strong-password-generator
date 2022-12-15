9666c9bfe594770f6b1f363efd8310e4dc50be0f
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-12-15 14:33:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- cdn  -->
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    <section class="bg-primary d-flex justify-content-center align-items-center container-fluid" style="height: 100vh;">
+        <div class="container-md bg-light py-5">
+        <form action="" method="get">
+            <label for="password-length">Inserisci numero caratteri password: </label>
+            <input name="password-length" type="text">
+            <button type="submit"> Crea</button>
+        </form>
+        </div>
+    </section>
+  
+</body>
+</html>
